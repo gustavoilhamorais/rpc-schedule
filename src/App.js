1bc2086d8e606b77570e2d9c94ef853c9bcf0794
gustavoilhamorais <gustavoilhamorais@gmail.com>
2020-11-19 23:31:21 -0300

    create-global-states-management

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Axios from 'axios';
+import { Provider } from './containers/GlobalContext';
+import Routes from './Routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/shards-dashboards.1.1.0.min.css';
 
 function App() {
+  const [schedule, setSchedule] = React.useState(() => []);
+  const [error, setError] = React.useState(() => "");
+  const [isLoading, setIsLoading] = React.useState(() => true);
+  const [isCardOpen, setIsCardOpen] = React.useState(() => { });
+  const [programsCategories, setProgramsCategories] = React.useState(() => []);
+  const [selectedCategory, setSelectedCategory] = React.useState(() => []);
+  const [currentDate, setCurrentDate] = React.useState(() => new Date());
+  const todaysDate = new Date().toISOString().split("T")[0];
+  function updateFilters({ target: { value } }) {
+    setSelectedCategory(() => value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider value={{
+      schedule,
+      error,
+      isLoading,
+      isCardOpen,
+      setIsCardOpen,
+      programsCategories,
+      updateFilters,
+      selectedCategory,
+      fetchScheduleGrid,
+      currentDate,
+      setCurrentDate
+    }}>
+      <Routes />
+    </Provider>
   );
 }
 
